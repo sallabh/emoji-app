07c908412ef157f406c39309ab944c4a2863387e
sallabh <sallabhk@gmail.com>
2019-11-11 20:47:57 +0530

    Updates

---
+++ precache-manifest.d168fde53dfdd585e1b712da1a930cda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d7f7898f7f1954c20ac756c1a2fbca",
+    "url": "/emoji-app/index.html"
+  },
+  {
+    "revision": "6f8283346fb2f70bf0a0",
+    "url": "/emoji-app/static/css/main.542f1a96.chunk.css"
+  },
+  {
+    "revision": "4e933e5b9bd63cae4bb6",
+    "url": "/emoji-app/static/js/2.67521505.chunk.js"
+  },
+  {
+    "revision": "6f8283346fb2f70bf0a0",
+    "url": "/emoji-app/static/js/main.0e426803.chunk.js"
+  },
+  {
+    "revision": "91ab8ebd7a3eb74a5024",
+    "url": "/emoji-app/static/js/runtime-main.9f932d53.js"
+  }
+]);
